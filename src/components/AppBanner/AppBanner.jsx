26241781ff94505bd5746e180e2521b2808cdefd
Etem Utku Oylum <utkuoylum@Etems-MacBook-Air.local>
2024-07-31 10:05:16 +0200

    banner section

--- src/components/AppBanner/AppBanner.jsx
+++ src/components/AppBanner/AppBanner.jsx
@@ -1,7 +1,50 @@
 import React from "react";
+import bannerImg from "/public/coffee-cover.jpg";
+import appStoreImg from "/public/website/app_store.png";
+import playStoreImg from "/public/website/play_store.png";
+import Image from "next/image";
+import { motion } from "framer-motion";
+import Link from "next/link";
+
+const BannerStyle = {
+  backgroundImage: "url('/coffee-cover.jpg')",
+  backgroundSize: "cover",
+  backgroundPosition: "center",
+  backgroundRepeat: "no-repeat",
+  height: "100%",
+  width: "100%",
+};
 
 const AppBanner = () => {
-  return <div>AppBanner</div>;
+  return (
+    <div className="container my-14">
+      <div
+        style={BannerStyle}
+        className="sm:min-h-[400px] sm:flex sm:justify-end sm:items-center rounded-xl"
+      >
+        <div>
+          <div className="space-y-6 max-w-xl mx-auto">
+            <p className="text-2xl text-center sm:text-4xl font-semibold">
+              Download the app
+            </p>
+            <p className="text-center sm:px-20">
+              Lorem ipsum dolor sit amet consectetur adipisicing elit. Eius
+              natus cumque esse, eum doloribus
+            </p>
+            {/* images link  */}
+            <div className="flex justify-evenly">
+              <Link href="#" className="max-w-[150px] sm:max-w-[120px] md:max-w-[200px]">
+                <Image src='/website/app_store.png' alt="app store image" width={200} height={300}/>
+              </Link>
+              <Link href="#" className="max-w-[150px] sm:max-w-[120px] md:max-w-[200px]">
+                <Image src={playStoreImg} alt="play store image" width={200} height={300}/>
+              </Link>
+            </div>
+          </div>
+        </div>
+      </div>
+    </div>
+  );
 };
 
 export default AppBanner;
--- src/components/WhereToBuy/WhereToBuy.jsx
+++ src/components/WhereToBuy/WhereToBuy.jsx
@@ -1,7 +1,84 @@
+'use client'
+import Image from "next/image";
 import React from "react";
+import worldMap from "/public/world-map.png";
+import { motion } from "framer-motion";
 
 const WhereToBuy = () => {
-  return <div>WhereToBuy</div>;
+  return (
+    <div className="container my-36">
+      <div className="grid grid-cols-1 sm:grid-cols-3 gap-8 place-items-center">
+        {/* form section  */}
+        <div className="space-y-8">
+          <motion.h2 
+          initial={{opacity:0, y:100}}
+          whileInView={{opacity:1, y:0}}
+          transition={{
+            type: 'spring',
+            stiffness: 100,
+            damping:10,
+            resDelta: 0.1,
+            delay: 0.2
+          }}
+          className="text-4xl font-bold text-darkGray font-serif">Buy our products from anywhere </motion.h2>
+          <motion.div 
+           initial={{opacity:0, y:100}}
+           whileInView={{opacity:1, y:0}}
+           transition={{
+            type: 'spring',
+            stiffness: 100,
+            damping:10,
+            resDelta: 0.1,
+             delay: 0.4
+           }}
+          className="flex items-center gap-4">
+            <input type="text" placeholder="Name" className="input-style w-full lg:w-[150px]"/>
+            <input type="email" placeholder="Email" className="input-style w-full"/>
+          </motion.div>
+          <motion.div 
+          initial={{opacity:0, y:100}}
+          whileInView={{opacity:1, y:0}}
+          transition={{
+            type: 'spring',
+            stiffness: 100,
+            damping:10,
+            resDelta: 0.1,
+            delay: 0.6
+          }}
+          className="flex items-center gap-4">
+            <input type="text" placeholder="Country" className="input-style w-full "/>
+            <input type="text" placeholder="Zipcode" className="input-style w-full lg:w-[150px]"/>
+          </motion.div>
+
+          <motion.button 
+          initial={{opacity:0, y:100}}
+          whileInView={{opacity:1, y:0}}
+          transition={{
+            type: 'spring',
+            stiffness: 100,
+            damping:10,
+            resDelta: 0.1,
+            delay: 0.8
+          }}
+          className="primary-btn w-full">Order Now</motion.button>
+        </div>
+        {/* map section  */}
+        <motion.div 
+        initial={{opacity:0, scale:0.5}}
+        whileInView={{opacity:1, scale:1}}
+        transition={{
+          type: 'spring',
+          stiffness: 100,
+          damping:10,
+          resDelta: 0.1,
+          delay: 1
+        }}
+        className="col-span-2">
+          <Image src={worldMap} alt="world map" className="w-full sm:w-[500px] mx-auto" />
+        </motion.div>
+      </div>
+    </div>
+  );
 };
 
 export default WhereToBuy;
